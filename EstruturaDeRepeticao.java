ce26befc1d72b217a79a67328d20b7d0d0867d5b
mariaizabelabarbosa <mariaizabelabarbosa11@gmail.com>
2023-02-13 11:27:22 +0000

    gravar todos os arquivos

--- EstruturaDeRepeticao.java
+++ EstruturaDeRepeticao.java
@@ -1,3 +1,11 @@
 public class EstruturaDeRepeticao {
+    public static void main (String[] args){ 
+    int cc = 0;
+    while (cc<4) {
+    }
+        System.out.println("teste" + (cc+1));
+        cc++;
+    }
+
 
 }
