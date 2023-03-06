8c7d64e69e27eb9352e3a5ef33cc8eb612b3f86b
mariaizabelabarbosa <mariaizabelabarbosa11@gmail.com>
2023-03-06 11:33:59 +0000

    salvar

--- InserirObjeto.java
+++ InserirObjeto.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+  public static void main(String[] args){        Scanner input = new Scanner(System.in);        double NovoSalario, AumentoSalario;               System.out.println("Informe seu salario: ");        NovoSalario = input.nextFloat();           if (NovoSalario >= 1000 && NovoSalario <= 2000)        {            AumentoSalario = NovoSalario * 1.15;            System.out.print("Aqui esta seu salario novo: " + AumentoSalario + "\n");        }           else if ( NovoSalario >= 2000 && NovoSalario <= 3000)        {            AumentoSalario = NovoSalario  * 1.10;            System.out.print("Aqui esta seu salario novo: " + AumentoSalario + "\n");        }           else if(NovoSalario > 3000)        {            AumentoSalario = NovoSalario * 1.5;            System.out.print("Aqui esta seu salario novo: " +  AumentoSalario + "\n");        }    }
 
 public class InserirObjeto {
 
